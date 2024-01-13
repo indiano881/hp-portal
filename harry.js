5a7f57e523f2101bdbc9ed8431723ac46a7529ad
Davide Baldi <d.baldi88@gmail.com>
2024-01-13 08:07:31 +0100

    cleaning up before break.NEXT is working on thr books, click on picture-display info?

--- harry.js
+++ harry.js
@@ -1,7 +1,7 @@
 $(() => {
     //books, movies, spells, potion, characters
     const API_URL="https://api.potterdb.com/v1/";
-    const API_ENDPOINTS =["books","movies","spells","potion","characters"];
+    const API_ENDPOINTS =["books","movies","spells"];
 
     const getData = async (url) => {
         
@@ -42,13 +42,12 @@ $(() => {
                 <img src=${spellImage} id="picture${index}">
                 `)
             } else {
-                $(".grid-container").append(`<img src="./images/magic.jpg" >`)
+                $(".grid-container").append(`<img src="./images/magic.jpg" id="picture${index}>`)
             }
-            
-
-            
         })
     }
+
+    
 
     
     
@@ -79,3 +78,35 @@ $(() => {
 
 
 })
+
+/*
+Instructions
+In this assignment we will create a webpage loaded with AJAX data returned from an API.
+
+This is to test your knowledge of JSON objects, AJAX, working with APIs and html/css/js. Vanilla JS, jQuery or both can be used to complete the assignment.
+
+The choice of API is up to you but please keep in mind any keys you might need. You can use XHR or fetch to access the data.
+
+The page needs to have at least one event that triggers the initial API call. This could be a 'start' button, or a 'search' field with submit or something similar. 
+
+On firing the event an AJAX request is made to an API and data is returned. The data must be a JSON object and not a simple string.
+
+The data that is returned from the call must be dynamically displayed on the page. You can populate pre-existing elements or dynamically create elements.
+
+The specifications will depend somewhat on your choice of API.
+
+Submit both a github pages link and a link to the repo.
+
+To achieve a Godkänt grade you must:
+connect to an API in a user fired event
+make a request for data
+handle the returned data in an efficient manner
+display more than one property of the returned data on the page >> DO IT!!!!
+the page must be responsive
+To achieve a Välgodkänt grade you must complete the above and:
+Have correct error handling when fetching the data
+Append arguments to the request
+Multiple calls to the API - eg have a input field that will generate different arguments and a button that fires the request
+Semantic code
+A consistent code style
+*/
