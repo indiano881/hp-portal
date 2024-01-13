f0a8b64973f82682b3628d4992964d27ade3bf73
Davide Baldi <d.baldi88@gmail.com>
2024-01-13 07:05:56 +0100

    movies function working - NEED TO FIX EMPTY()

--- harry.js
+++ harry.js
@@ -7,29 +7,41 @@ $(() => {
         let response = await fetch(url);
         let data = await response.json();
         console.log(data);
-        renderBooks(data);
+        //renderBooks(data);
+        renderMovies(data);
     }
-
 
     const renderBooks = books => {
         let booksArray=books.data;
         $(booksArray).map((index, book)=> {
-
             let coverImage= book.attributes.cover;//imagine cover
-            $(".grid-container").empty().append(`
+            $(".grid-container").append(`
             <img src=${coverImage} id="picture${index}">
             `)
-
-
         })
-
-
-        
     }
+
+    const renderMovies = movie => {
+        let moviesArray=movie.data;
+        $(moviesArray).map((index, movie)=> {
+            let posterImage= movie.attributes.poster;//imagine cover
+            $(".grid-container").append(`
+            <img src=${posterImage} id="picture${index}">
+            `)
+        })
+    }
+
     
+    
+
+
+
+
+
+
     
     $(".searchBtn").on("click", ()=> {
-        let userInput=$("#search-bar").val();//inserire regex??
+        let userInput=$("#search-bar").val();//inserire regex?? o convertire in singoli bottoni??
         getData(API_URL+userInput);
         
     })
